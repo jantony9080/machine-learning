c8f748f96bcd74463cbc464825001d790bfd5ee9
Anatnom90 <jantony9080@gmail.com>
2018-03-14 16:03:28 +0300

    add learning-svm module

--- process-manager/src/main/java/ru/titus/manager/controller/MainController.java
+++ process-manager/src/main/java/ru/titus/manager/controller/MainController.java
@@ -3,20 +3,24 @@ package ru.titus.manager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.titus.learning.svm.SvmPredict;
+import ru.titus.learning.svm.SvmTrain;
 import ru.titus.manager.service.DataCollector;
-import ru.titus.learning.neuroph.MyNeuralNetwork;
 
 @RestController
 public class MainController {
 
-    @Autowired
-    private DataCollector dataCollector;
+    @Autowired private DataCollector dataCollector;
+    @Autowired private SvmTrain svmTrain;
+    @Autowired private SvmPredict svmPredict;
 
     @RequestMapping("/run")
     public void start() {
 //        MyNeuralNetwork myNeuralNetwork = new MyNeuralNetwork();
-////        myNeuralNetwork.start();
-        dataCollector.collectMatchOdds( );
+////      myNeuralNetwork.start();
+//        dataCollector.collectMatchOdds( );
+        svmTrain.run();
+        svmPredict.run();
         System.out.println("All worked");
     }
 
